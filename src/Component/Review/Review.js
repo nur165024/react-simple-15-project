0da7be5623566dd4d822033978d332e40115d953
nur165024 <nur42658@gmail.com>
2021-06-09 00:29:34 +0700

    user review done

--- src/Component/Review/Review.js
+++ src/Component/Review/Review.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import reviewData from './ReviewData';
+import { FaAngleLeft,FaAngleRight } from "react-icons/fa";
 
 const Review = () => {
+    const[index,setIndex] = useState(0);
+    const {name,job,image,text} = reviewData[index];
+
+    const checkNumber = (number) => {
+        if (number > reviewData.length - 1) {
+          return 0;
+        }
+        if (number < 0) {
+          return reviewData.length - 1;
+        }
+        return number;
+    };
+
+    const leftHandleClick = () => {
+        setIndex((index) => {
+            let newIndex = index - 1;
+            console.log(newIndex);
+            return checkNumber(newIndex);
+        })
+    }
+
+    const rightHandleClick = () => {
+        setIndex((index) => {
+            let newIndex = index + 1;
+            console.log(newIndex);
+            return checkNumber(newIndex);
+        })
+    }
+
     return (
         <section>
             <div className="container">
@@ -10,9 +41,23 @@ const Review = () => {
                         <h1>User Reivew</h1>
                         <Link className="btn btn-success mb-3" to="/">Back</Link>
                     </div>
-                    <hr />
+                </div>
+                <hr />
+                <div className="row justify-content-center">
                     <div className="col-md-8">
-                        <p>user review</p>
+                        <div class="card text-center">
+                            <div class="card-body">
+                                <img style={{ width:'auto',height:'100px',borderRadius:'10%' }} src={image} alt="" />
+                                <h5 class="card-title mt-3">{name}</h5>
+                                <h6 className="text-primary">{job}</h6>
+                                <p class="card-text">{text}</p>
+                                <p>
+                                    <button onClick={leftHandleClick} className="btn btn-link"><FaAngleLeft></FaAngleLeft></button>
+                                    <button onClick={rightHandleClick} className="btn btn-link"><FaAngleRight></FaAngleRight></button>
+                                </p>
+                                <Link href="#" class="btn btn-primary">Suprise Me</Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
